a5912b2393b2a3c5340ebdf1c5c14f1cfd9da794
Graham heeney <20102466@mail.wit.ie>
2024-12-03 19:44:48 +0000

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -4,7 +4,18 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true,
+    validate: {
+      validator: function (value) {
+        return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(value);
+      },
+      message: 'Password must be at least 8 characters long and include at least one letter, one number, and one special character.',
+    },
+  },
 });
+
+UserSchema.index({ username: 1 }, { unique: true });
+
+
 
 export default mongoose.model('User', UserSchema);
